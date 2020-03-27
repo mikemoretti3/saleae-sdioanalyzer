fada774182504e83e216690425fd299dc5128f50
Mike Moretti <mike@mordent.com>
2020-03-27 18:06:44 -0400

    Add parsing of cmd 52 response

--- source/SDIOAnalyzerResults.cpp
+++ source/SDIOAnalyzerResults.cpp
@@ -27,6 +27,7 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
+#include <stdarg.h>
 
 SDIOAnalyzerResults::SDIOAnalyzerResults( SDIOAnalyzer* analyzer, SDIOAnalyzerSettings* settings )
 :	AnalyzerResults(),
@@ -37,6 +38,19 @@ SDIOAnalyzerResults::SDIOAnalyzerResults( SDIOAnalyzer* analyzer, SDIOAnalyzerSe
 
 SDIOAnalyzerResults::~SDIOAnalyzerResults()
 {
+}
+
+static void logDbg(char* fmt, ...)
+{
+	va_list args;
+	FILE* lf = fopen("logic.log", "a");
+	if (!lf) {
+		return;
+	}
+	va_start(args, fmt);
+	vfprintf(lf, fmt, args);
+	va_end(args);
+	fclose(lf);
 }
 
 #define CMD52_WR_DATA 0
@@ -50,6 +64,82 @@ SDIOAnalyzerResults::~SDIOAnalyzerResults()
 #define CMD52_RW_FLAG (CMD52_FUNC+3)
 #define CMD52_RW_FLAG_MASK 1
 
+static std::stringstream ParseCmd52(Frame& frame, DisplayBase display_base)
+{
+	std::stringstream result;
+	char number_str1[128];
+	uint8_t rw = (frame.mData1 >> CMD52_RW_FLAG) & CMD52_RW_FLAG_MASK;
+	uint8_t raw = (frame.mData1 >> CMD52_RAW) & CMD52_RAW_MASK;
+	if (!rw) {
+		result << "R ";
+	}
+	else {
+		result << "W";
+		if (raw) {
+			result << "+R";
+		}
+		result << " ";
+	}
+	uint32_t func = (frame.mData1 >> CMD52_FUNC) & CMD52_FUNC_MASK;
+	AnalyzerHelpers::GetNumberString(func, Decimal, 0, number_str1, 128);
+	result << "F:" << number_str1 << " ";
+	uint32_t reg = (frame.mData1 >> CMD52_REG_ADDR) & CMD52_REG_ADDR_MASK;
+	AnalyzerHelpers::GetNumberString(reg, display_base, 0, number_str1, 128);
+	result << "REG:" << number_str1 << " ";
+	if (rw) {
+		uint8_t wrdata = (frame.mData1 >> CMD52_WR_DATA) & CMD52_WR_DATA_MASK;
+		AnalyzerHelpers::GetNumberString(wrdata, display_base, 0, number_str1, 128);
+		result << "DATA:" << number_str1 << " ";
+	}
+//	logDbg("Result: %s\n", result.str().c_str());
+	return result;
+}
+
+#define CMD52R5_DATA (0)
+#define CMD52R5_DATA_MASK 0xFF
+#define CMD52R5_RESP_FLAGS (8)
+#define CMD52R5_RESP_FLAGS_MASK 0xFF
+
+static std::stringstream ParseCmd52Resp(Frame& frame, DisplayBase display_base)
+{
+	std::stringstream result;
+	char number_str1[128];
+
+	uint8_t flags = (frame.mData1 >> CMD52R5_RESP_FLAGS) & CMD52R5_RESP_FLAGS_MASK;
+	result << "F:";
+	if (flags & (1<<7)) {
+		result << "CRC ";
+	}
+	if (flags & (1<<6)) {
+		result << "ILL ";
+	}
+	uint8_t state = (flags & (3<<4)) >> 4;
+	switch (state) {
+	case 0b00:
+		result << "DIS ";
+		break;
+	case 0b01:
+		result << "CMD ";
+		break;
+	case 0b10:
+		result << "TRN ";
+		break;
+	}
+	if (flags & (1<<3)) {
+		result << "ERR ";
+	}
+	if (flags & (1<<1)) {
+		result << "FUN ";
+	}
+	if (flags & 1) {
+		result << "OOR ";
+	}
+	uint8_t data = (frame.mData1 >> CMD52R5_DATA) & CMD52R5_DATA_MASK;
+	AnalyzerHelpers::GetNumberString(data, display_base, 0, number_str1, 128);
+	result << "DATA: " << number_str1 << " ";
+
+	return result;
+}
 
 void SDIOAnalyzerResults::GenerateBubbleText( U64 frame_index, Channel& channel, DisplayBase display_base )
 {
@@ -76,37 +166,21 @@ void SDIOAnalyzerResults::GenerateBubbleText( U64 frame_index, Channel& channel,
 		AnalyzerHelpers::GetNumberString( frame.mData1, Decimal, 6, number_str1, 128 );
 		AddResultString("CMD ", number_str1);
 		lastCmd = frame.mData1;
-	}else if (frame.mType == SDIOAnalyzer::FRAME_ARG){
+	}
+	else if (frame.mType == SDIOAnalyzer::FRAME_ARG) {
 		std::stringstream result;
-#ifdef PARSE_SDIO_CMDS
-		if (lastCmd == 0x52) {
+		logDbg("LastCmd=%d\n", lastCmd);
+		if (lastCmd == 52) {
+//			logDbg("Cmd 52\n");
 			// host
 			if (!lastDir) {
-				uint8_t rw = (frame.mData1 >> CMD52_RW_FLAG) & CMD52_RW_FLAG_MASK;
-				uint8_t raw = (frame.mData1 >> CMD52_RAW) & CMD52_RAW_MASK;
-				if (rw) {
-					result << "R,";
-				}
-				else {
-					result << "W,";
-					if (raw) {
-						result << "RAW,";
-					}
-				}
-				uint32_t func = (frame.mData1 >> CMD52_FUNC) & CMD52_FUNC_MASK;
-				AnalyzerHelpers::GetNumberString(func, display_base, 32, number_str1, 128);
-				result << "F:" << number_str1 << ",";
-				uint32_t reg = (frame.mData1 >> CMD52_REG_ADDR) & CMD52_REG_ADDR_MASK;
-				AnalyzerHelpers::GetNumberString(reg, display_base, 32, number_str1, 128);
-				result << "REG:" << number_str1 << ",";
-				if (rw) {
-					uint8_t wrdata = (frame.mData1 >> CMD52_WR_DATA) & CMD52_WR_DATA_MASK;
-					AnalyzerHelpers::GetNumberString(wrdata, display_base, 32, number_str1, 128);
-					result << "DATA:" << number_str1 << " - ";
-				}
+//				logDbg("Host\n");
+				result = ParseCmd52(frame, display_base);
+			}
+			else {
+				result = ParseCmd52Resp(frame, display_base);
 			}
 		}
-#endif
 		AnalyzerHelpers::GetNumberString( frame.mData1, display_base, 32, number_str1, 128 );
 		result << "ARG:" << number_str1;
 		AddResultString(result.str().c_str());
@@ -114,7 +188,6 @@ void SDIOAnalyzerResults::GenerateBubbleText( U64 frame_index, Channel& channel,
 		AnalyzerHelpers::GetNumberString (frame.mData1, display_base, 64, number_str1, 128);
 		AnalyzerHelpers::GetNumberString (frame.mData2, display_base, 64, number_str2, 128);
 		AddResultString("LONG: ", number_str1, number_str2);
-
 	}else if (frame.mType == SDIOAnalyzer::FRAME_CRC){
 		AnalyzerHelpers::GetNumberString( frame.mData1, display_base, 7, number_str1, 128 );
 		AddResultString("CRC ", number_str1);
